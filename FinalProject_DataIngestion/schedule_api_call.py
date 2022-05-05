18a556275b0f9c32b683c60493a50b9f6afcbd67
ns5ub <ns5ub@virginia.edu>
2022-05-05 16:10:31 -0400

    fixed_api_scheduler

--- FinalProject_DataIngestion/schedule_api_call.py
+++ FinalProject_DataIngestion/schedule_api_call.py
@@ -13,34 +13,37 @@ headers = {
 }
 
 now = datetime.datetime.now()
-job_start = now + datetime.timedelta(hours=1)
-job_start_string = job_start.strftime("%H") + ":00:00"
+
+job_start = now.replace(minute=59, second=0)
+job_start_string = job_start.strftime("%H:%M:%S")
 print(job_start_string)
-job_start_string = "15:17:00"
 
 conn = sqlite3.connect('api_data.db') #creates new db or connects to existing
 cur = conn.cursor() #cursor to run queries
-cur.execute("""CREATE TABLE IF NOT EXISTS calls_test2(
+cur.execute("""CREATE TABLE IF NOT EXISTS calls(
    factor INTEGER,
    pi REAL,
    time TEXT);
 """)
 conn.commit() # Commit changes to connection
 
-
 def job():
     api_call = requests.request("GET", url, headers=headers, params=querystring)
     api_call_json = api_call.json()
     #print(api_call_json)
     resp = (api_call_json['factor'], api_call_json['pi'], api_call_json['time'])  # python tuple
-    cur.execute("INSERT INTO calls_test2 VALUES(?, ?, ?);", resp)
+    cur.execute("INSERT INTO calls VALUES(?, ?, ?);", resp)
     conn.commit()
 
 
-schedule.every(10).seconds.at(job_start_string).until(timedelta(minutes=3)).do(job)
+def job_that_executes_once():
+    schedule.every(1).minute.until(timedelta(hours=1)).do(job)
+    return schedule.CancelJob
 
-'''
+
+schedule.every().day.at(job_start_string).do(job_that_executes_once)
+#schedule.every(1).minutes.at(job_start_string).until(timedelta(hours=1)).do(job)
+
 while True:
     schedule.run_pending()
     time.sleep(1)
-'''
